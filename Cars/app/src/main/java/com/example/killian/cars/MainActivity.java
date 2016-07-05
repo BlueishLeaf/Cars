4e96b9bc145fd5a733aeec9ad5ff8e17f1f9b06f
BlueishLeaf <killiank360@gmail.com>
2016-07-05 15:59:49 +0100

    Added the initial UI

--- Cars/app/src/main/java/com/example/killian/cars/MainActivity.java
+++ Cars/app/src/main/java/com/example/killian/cars/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.killian.cars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 //This class is used to populate our collection view of cars
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Person person = new Person(23, "John", "Blue");
+        Log.i(person.getname(),"000000000000000000000000000000000");
     }
 }
