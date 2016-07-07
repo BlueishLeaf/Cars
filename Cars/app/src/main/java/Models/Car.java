db29d6800f066ee9bc1df2eca7ea59a51676dd07
BlueishLeaf <killiank360@gmail.com>
2016-07-07 10:15:48 +0100

    Renamed the two car models and wrote the code for the Car model

--- Cars/app/src/main/java/Models/Car.java
+++ Cars/app/src/main/java/Models/Car.java
@@ -1,7 +1,54 @@
 package Models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Killian on 06/07/2016.
  */
+
 public class Car {
+
+    //Car is the framework for the information page of each car in the catalog
+    //Attributes for a car's id, model, color, description, price, and image URL
+
+    private int id;
+    private String model;
+    private String color;
+    private String description;
+    private Double price;
+    private ArrayList<String> urls;
+
+    public Car(final int id, final String model, final String color, final String description, final Double price, final ArrayList<String>urls){
+
+        this.id = id;
+        this.model = model;
+        this.color = color;
+        this.description = description;
+        this.price = price;
+        this.urls = urls;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String model(){
+        return model;
+    }
+
+    public String color(){
+        return color;
+    }
+
+    public String description(){
+        return description;
+    }
+
+    public double price(){
+        return price;
+    }
+
+    public ArrayList<String> urls(){
+        return urls;
+    }
 }
---
+++ Cars/app/src/main/java/Models/CarItem.java
@@ -0,0 +1,33 @@
+package Models;
+
+/**
+ * Created by Killian on 06/07/2016.
+ */
+public class CarItem {
+
+    //CarItem is the framework for the cards of cars in the catalog
+    //CarItem will have an id, an image URL, and a model
+
+    private int id;
+    private String model;
+    private String url;
+
+    public CarItem(final int id, final String model, final String url){
+
+        this.id = id;
+        this.model = model;
+        this.url = url;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+}
