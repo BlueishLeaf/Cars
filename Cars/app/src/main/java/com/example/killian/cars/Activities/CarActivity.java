92cb3133d1257dccd43c2f5cd5cfef70693bdb71
BlueishLeaf <killiank360@gmail.com>
2016-07-12 16:57:24 +0100

    Tabs broke :(((((

--- Cars/app/src/main/java/com/example/killian/cars/Activities/CarActivity.java
+++ Cars/app/src/main/java/com/example/killian/cars/Activities/CarActivity.java
@@ -4,14 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.AnimationUtils;
 
 import com.example.killian.cars.Adapters.CarActivityFragmentAdapter;
 import com.example.killian.cars.Adapters.CarActivityPagerAdapter;
 import com.example.killian.cars.Constants.DBConstants;
 import com.example.killian.cars.Listeners.TabLayoutListener;
+import com.example.killian.cars.Models.Car;
 import com.example.killian.cars.R;
 import com.example.killian.cars.db.SQLiteHelper;
+
+import butterknife.BindView;
 
 /**
  * @author Killian.
@@ -19,10 +21,12 @@ import com.example.killian.cars.db.SQLiteHelper;
  */
 
 public class CarActivity extends AppCompatActivity {
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-    private ViewPager tabViewPager;
+
+    @BindView(R.id.viewPager) ViewPager viewPager;
+    @BindView(R.id.tab_layout) TabLayout tabLayout;
+    @BindView(R.id.carInfoPager) ViewPager tabViewPager;
     private int bundle_id;
+    private Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +35,15 @@ public class CarActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         initBundleVariables(getIntent().getExtras());
         SQLiteHelper db = new SQLiteHelper(this);
-        db.getCar(bundle_id);
+        car=db.getCar(bundle_id);
 
         initCarFragmentTabs();
         initPagerAdapter();
     }
     private void initPagerAdapter() {
-        CarActivityPagerAdapter adapter = new CarActivityPagerAdapter(this, image_urls);
+        CarActivityPagerAdapter adapter = new CarActivityPagerAdapter(this, car.urls());
         viewPager.setAdapter(adapter);
-        viewPager.setAnimation(AnimationUtils.get_slide_down(this, 500, 500));
+        //viewPager.setAnimation(AnimationUtils.get_slide_down(this, 500, 500));
     }
 
     private void initCarFragmentTabs() {
@@ -50,7 +54,7 @@ public class CarActivity extends AppCompatActivity {
                 (getSupportFragmentManager(), tabLayout.getTabCount());
         tabViewPager.setAdapter(adapter);
         tabViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setSelectedTabIndicatorColor(imageDetailColor);
+        //tabLayout.setSelectedTabIndicatorColor(imageDetailColor);
         tabLayout.addOnTabSelectedListener(new TabLayoutListener(tabViewPager));
 
     }
